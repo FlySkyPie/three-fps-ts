243baa49ff025ad698ead22ffa20f2fcee382854
Wei Ji (FlySkyPie) <c445dj544@gmail.com>
2024-05-13 21:25:53 +0800

    refactor: Convert file to typescript

--- src/entry.ts
+++ src/entry.ts
@@ -1,79 +1,104 @@
 /**
  * entry.js
- * 
- * This is the first file loaded. It sets up the Renderer, 
- * Scene, Physics and Entities. It also starts the render loop and 
+ *
+ * This is the first file loaded. It sets up the Renderer,
+ * Scene, Physics and Entities. It also starts the render loop and
  * handles window resizes.
- * 
+ *
  */
 
-import * as THREE from 'three'
-import { AmmoHelper, Ammo, createConvexHullShape } from './AmmoLib'
-import EntityManager from './EntityManager'
-import Entity from './Entity'
-import Sky from './entities/Sky/Sky2'
-import LevelSetup from './entities/Level/LevelSetup'
-import PlayerControls from './entities/Player/PlayerControls'
-import PlayerPhysics from './entities/Player/PlayerPhysics'
-import Stats from 'three/examples/jsm/libs/stats.module'
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
-import { SkeletonUtils } from 'three/examples/jsm/utils/SkeletonUtils'
-import NpcCharacterController from './entities/NPC/CharacterController'
-import Input from './Input'
+import * as THREE from "three";
+import Stats from "three/examples/jsm/libs/stats.module";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { SkeletonUtils } from "three/examples/jsm/utils/SkeletonUtils";
 
-import level from './assets/level.glb?url'
-import navmesh from './assets/navmesh.obj?url'
+import { AmmoHelper, Ammo, createConvexHullShape } from "./AmmoLib";
+import EntityManager from "./EntityManager";
+import Entity from "./Entity";
+import Sky from "./entities/Sky/Sky2";
+import LevelSetup from "./entities/Level/LevelSetup";
+import PlayerControls from "./entities/Player/PlayerControls";
+import PlayerPhysics from "./entities/Player/PlayerPhysics";
 
-import mutant from './assets/animations/mutant.fbx?url'
-import idleAnim from './assets/animations/mutant breathing idle.fbx?url'
-import attackAnim from './assets/animations/mutant punch.fbx?url'
-import walkAnim from './assets/animations/mutant walking.fbx?url'
-import runAnim from './assets/animations/mutant run.fbx?url'
-import dieAnim from './assets/animations/mutant dying.fbx?url'
+import NpcCharacterController from "./entities/NPC/CharacterController";
+import Input from "./Input";
+
+import level from "./assets/level.glb?url";
+import navmesh from "./assets/navmesh.obj?url";
+
+import mutant from "./assets/animations/mutant.fbx?url";
+import idleAnim from "./assets/animations/mutant breathing idle.fbx?url";
+import attackAnim from "./assets/animations/mutant punch.fbx?url";
+import walkAnim from "./assets/animations/mutant walking.fbx?url";
+import runAnim from "./assets/animations/mutant run.fbx?url";
+import dieAnim from "./assets/animations/mutant dying.fbx?url";
 
 //AK47 Model and textures
-import ak47 from './assets/guns/ak47/ak47.glb?url'
-import muzzleFlash from './assets/muzzle_flash.glb?url'
+import ak47 from "./assets/guns/ak47/ak47.glb?url";
+import muzzleFlash from "./assets/muzzle_flash.glb?url";
 //Shot sound
-import ak47Shot from './assets/sounds/ak47_shot.wav?url'
+import ak47Shot from "./assets/sounds/ak47_shot.wav?url";
 
 //Ammo box
-import ammobox from './assets/ammo/AmmoBox.fbx?url'
-import ammoboxTexD from './assets/ammo/AmmoBox_D.tga.png?url'
-import ammoboxTexN from './assets/ammo/AmmoBox_N.tga.png?url'
-import ammoboxTexM from './assets/ammo/AmmoBox_M.tga.png?url'
-import ammoboxTexR from './assets/ammo/AmmoBox_R.tga.png?url'
-import ammoboxTexAO from './assets/ammo/AmmoBox_AO.tga.png?url'
+import ammobox from "./assets/ammo/AmmoBox.fbx?url";
+import ammoboxTexD from "./assets/ammo/AmmoBox_D.tga.png?url";
+import ammoboxTexN from "./assets/ammo/AmmoBox_N.tga.png?url";
+import ammoboxTexM from "./assets/ammo/AmmoBox_M.tga.png?url";
+import ammoboxTexR from "./assets/ammo/AmmoBox_R.tga.png?url";
+import ammoboxTexAO from "./assets/ammo/AmmoBox_AO.tga.png?url";
 
 //Bullet Decal
-import decalColor from './assets/decals/decal_c.jpg?url'
-import decalNormal from './assets/decals/decal_n.jpg?url'
-import decalAlpha from './assets/decals/decal_a.jpg?url'
+import decalColor from "./assets/decals/decal_c.jpg?url";
+import decalNormal from "./assets/decals/decal_n.jpg?url";
+import decalAlpha from "./assets/decals/decal_a.jpg?url";
 
 //Sky
-import skyTex from './assets/sky.jpg?url'
+import skyTex from "./assets/sky.jpg?url";
 
-import DebugDrawer from './DebugDrawer'
-import Navmesh from './entities/Level/Navmesh'
-import AttackTrigger from './entities/NPC/AttackTrigger'
-import DirectionDebug from './entities/NPC/DirectionDebug'
-import CharacterCollision from './entities/NPC/CharacterCollision'
-import Weapon from './entities/Player/Weapon'
-import UIManager from './entities/UI/UIManager'
-import AmmoBox from './entities/AmmoBox/AmmoBox'
-import LevelBulletDecals from './entities/Level/BulletDecals'
-import PlayerHealth from './entities/Player/PlayerHealth'
+import DebugDrawer from "./DebugDrawer";
+import Navmesh from "./entities/Level/Navmesh";
+import AttackTrigger from "./entities/NPC/AttackTrigger";
+import DirectionDebug from "./entities/NPC/DirectionDebug";
+import CharacterCollision from "./entities/NPC/CharacterCollision";
+import Weapon from "./entities/Player/Weapon";
+import UIManager from "./entities/UI/UIManager";
+import AmmoBox from "./entities/AmmoBox/AmmoBox";
+import LevelBulletDecals from "./entities/Level/BulletDecals";
+import PlayerHealth from "./entities/Player/PlayerHealth";
 
 class FPSGameApp {
+  lastFrameTime: any;
+
+  assets: any;
+
+  animFrameId: any;
+
+  scene: any;
+
+  renderer: any;
+
+  stats: any;
+
+  camera: any;
+
+  listener: any;
+
+  physicsWorld: any;
+
+  mutantAnims: any;
+
+  entityManager: any;
 
   constructor() {
     this.lastFrameTime = null;
     this.assets = {};
     this.animFrameId = 0;
 
-    AmmoHelper.Init(() => { this.Init(); });
+    AmmoHelper.Init(() => {
+      this.Init();
+    });
   }
 
   Init() {
@@ -103,7 +128,7 @@ class FPSGameApp {
     this.renderer.setPixelRatio(window.devicePixelRatio);
 
     this.WindowResizeHanlder();
-    window.addEventListener('resize', this.WindowResizeHanlder);
+    window.addEventListener("resize", this.WindowResizeHanlder);
 
     document.body.appendChild(this.renderer.domElement);
 
@@ -118,23 +143,31 @@ class FPSGameApp {
     const dispatcher = new Ammo.btCollisionDispatcher(collisionConfiguration);
     const broadphase = new Ammo.btDbvtBroadphase();
     const solver = new Ammo.btSequentialImpulseConstraintSolver();
-    this.physicsWorld = new Ammo.btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
+    this.physicsWorld = new Ammo.btDiscreteDynamicsWorld(
+      dispatcher,
+      broadphase,
+      solver,
+      collisionConfiguration
+    );
     this.physicsWorld.setGravity(new Ammo.btVector3(0.0, -9.81, 0.0));
     const fp = Ammo.addFunction(this.PhysicsUpdate);
     this.physicsWorld.setInternalTickCallback(fp);
-    this.physicsWorld.getBroadphase().getOverlappingPairCache().setInternalGhostPairCallback(new Ammo.btGhostPairCallback());
+    this.physicsWorld
+      .getBroadphase()
+      .getOverlappingPairCache()
+      .setInternalGhostPairCallback(new Ammo.btGhostPairCallback());
 
     //Physics debug drawer
     //this.debugDrawer = new DebugDrawer(this.scene, this.physicsWorld);
     //this.debugDrawer.enable();
   }
 
-  SetAnim(name, obj) {
+  SetAnim(name: any, obj: any) {
     const clip = obj.animations[0];
     this.mutantAnims[name] = clip;
   }
 
-  PromiseProgress(proms, progress_cb) {
+  PromiseProgress(proms: any, progress_cb: any) {
     let d = 0;
     progress_cb(0);
     for (const p of proms) {
@@ -146,14 +179,14 @@ class FPSGameApp {
     return Promise.all(proms);
   }
 
-  AddAsset(asset, loader, name) {
-    return loader.loadAsync(asset).then(result => {
+  AddAsset(asset: any, loader: any, name: any) {
+    return loader.loadAsync(asset).then((result: any) => {
       this.assets[name] = result;
     });
   }
 
-  OnProgress(p) {
-    const progressbar = document.getElementById('progress');
+  OnProgress(p: any) {
+    const progressbar = document.getElementById("progress")!;
     progressbar.style.width = `${p}%`;
   }
 
@@ -162,11 +195,15 @@ class FPSGameApp {
   }
 
   SetupStartButton() {
-    document.getElementById('start_game').addEventListener('click', this.StartGame);
+    document
+      .getElementById("start_game")!
+      .addEventListener("click", this.StartGame);
   }
 
   ShowMenu(visible = true) {
-    document.getElementById('menu').style.visibility = visible ? 'visible' : 'hidden';
+    document.getElementById("menu")!.style.visibility = visible
+      ? "visible"
+      : "hidden";
   }
 
   async LoadAssets() {
@@ -207,38 +244,37 @@ class FPSGameApp {
 
     await this.PromiseProgress(promises, this.OnProgress);
 
-    this.assets['level'] = this.assets['level'].scene;
-    this.assets['muzzleFlash'] = this.assets['muzzleFlash'].scene;
+    this.assets["level"] = this.assets["level"].scene;
+    this.assets["muzzleFlash"] = this.assets["muzzleFlash"].scene;
 
     //Extract mutant anims
     this.mutantAnims = {};
-    this.SetAnim('idle', this.assets['idleAnim']);
-    this.SetAnim('walk', this.assets['walkAnim']);
-    this.SetAnim('run', this.assets['runAnim']);
-    this.SetAnim('attack', this.assets['attackAnim']);
-    this.SetAnim('die', this.assets['dieAnim']);
+    this.SetAnim("idle", this.assets["idleAnim"]);
+    this.SetAnim("walk", this.assets["walkAnim"]);
+    this.SetAnim("run", this.assets["runAnim"]);
+    this.SetAnim("attack", this.assets["attackAnim"]);
+    this.SetAnim("die", this.assets["dieAnim"]);
 
-    this.assets['ak47'].scene.animations = this.assets['ak47'].animations;
+    this.assets["ak47"].scene.animations = this.assets["ak47"].animations;
 
     //Set ammo box textures and other props
-    this.assets['ammobox'].scale.set(0.01, 0.01, 0.01);
-    this.assets['ammobox'].traverse(child => {
+    this.assets["ammobox"].scale.set(0.01, 0.01, 0.01);
+    this.assets["ammobox"].traverse((child: any) => {
       child.castShadow = true;
       child.receiveShadow = true;
 
       child.material = new THREE.MeshStandardMaterial({
-        map: this.assets['ammoboxTexD'],
-        aoMap: this.assets['ammoboxTexAO'],
-        normalMap: this.assets['ammoboxTexN'],
+        map: this.assets["ammoboxTexD"],
+        aoMap: this.assets["ammoboxTexAO"],
+        normalMap: this.assets["ammoboxTexN"],
         metalness: 1,
-        metalnessMap: this.assets['ammoboxTexM'],
-        roughnessMap: this.assets['ammoboxTexR'],
-        color: new THREE.Color(0.4, 0.4, 0.4)
+        metalnessMap: this.assets["ammoboxTexM"],
+        roughnessMap: this.assets["ammoboxTexR"],
+        color: new THREE.Color(0.4, 0.4, 0.4),
       });
-
     });
 
-    this.assets['ammoboxShape'] = createConvexHullShape(this.assets['ammobox']);
+    this.assets["ammoboxShape"] = createConvexHullShape(this.assets["ammobox"]);
 
     this.HideProgress();
     this.ShowMenu();
@@ -248,36 +284,64 @@ class FPSGameApp {
     this.entityManager = new EntityManager();
 
     const levelEntity = new Entity();
-    levelEntity.SetName('Level');
-    levelEntity.AddComponent(new LevelSetup(this.assets['level'], this.scene, this.physicsWorld));
-    levelEntity.AddComponent(new Navmesh(this.scene, this.assets['navmesh']));
-    levelEntity.AddComponent(new LevelBulletDecals(this.scene, this.assets['decalColor'], this.assets['decalNormal'], this.assets['decalAlpha']));
+    levelEntity.SetName("Level");
+    levelEntity.AddComponent(
+      new LevelSetup(this.assets["level"], this.scene, this.physicsWorld)
+    );
+    levelEntity.AddComponent(new Navmesh(this.scene, this.assets["navmesh"]));
+    levelEntity.AddComponent(
+      new LevelBulletDecals(
+        this.scene,
+        this.assets["decalColor"],
+        this.assets["decalNormal"],
+        this.assets["decalAlpha"]
+      )
+    );
     this.entityManager.Add(levelEntity);
 
     const skyEntity = new Entity();
     skyEntity.SetName("Sky");
-    skyEntity.AddComponent(new Sky(this.scene, this.assets['skyTex']));
+    skyEntity.AddComponent(new Sky(this.scene, this.assets["skyTex"]));
     this.entityManager.Add(skyEntity);
 
     const playerEntity = new Entity();
     playerEntity.SetName("Player");
     playerEntity.AddComponent(new PlayerPhysics(this.physicsWorld, Ammo));
-    playerEntity.AddComponent(new PlayerControls(this.camera, this.scene));
-    playerEntity.AddComponent(new Weapon(this.camera, this.assets['ak47'].scene, this.assets['muzzleFlash'], this.physicsWorld, this.assets['ak47Shot'], this.listener));
+    playerEntity.AddComponent(new PlayerControls(this.camera));
+    playerEntity.AddComponent(
+      new Weapon(
+        this.camera,
+        this.assets["ak47"].scene,
+        this.assets["muzzleFlash"],
+        this.physicsWorld,
+        this.assets["ak47Shot"],
+        this.listener
+      )
+    );
     playerEntity.AddComponent(new PlayerHealth());
     playerEntity.SetPosition(new THREE.Vector3(2.14, 1.48, -1.36));
-    playerEntity.SetRotation(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), -Math.PI * 0.5));
+    playerEntity.SetRotation(
+      new THREE.Quaternion().setFromAxisAngle(
+        new THREE.Vector3(0, 1, 0),
+        -Math.PI * 0.5
+      )
+    );
     this.entityManager.Add(playerEntity);
 
-    const npcLocations = [
-      [10.8, 0.0, 22.0],
-    ];
+    const npcLocations = [[10.8, 0.0, 22.0]];
 
     npcLocations.forEach((v, i) => {
       const npcEntity = new Entity();
       npcEntity.SetPosition(new THREE.Vector3(v[0], v[1], v[2]));
       npcEntity.SetName(`Mutant${i}`);
-      npcEntity.AddComponent(new NpcCharacterController(SkeletonUtils.clone(this.assets['mutant']), this.mutantAnims, this.scene, this.physicsWorld));
+      npcEntity.AddComponent(
+        new NpcCharacterController(
+          SkeletonUtils.clone(this.assets["mutant"]),
+          this.mutantAnims,
+          this.scene,
+          this.physicsWorld
+        )
+      );
       npcEntity.AddComponent(new AttackTrigger(this.physicsWorld));
       npcEntity.AddComponent(new CharacterCollision(this.physicsWorld));
       npcEntity.AddComponent(new DirectionDebug(this.scene));
@@ -297,7 +361,14 @@ class FPSGameApp {
     ammoLocations.forEach((loc, i) => {
       const box = new Entity();
       box.SetName(`AmmoBox${i}`);
-      box.AddComponent(new AmmoBox(this.scene, this.assets['ammobox'].clone(), this.assets['ammoboxShape'], this.physicsWorld));
+      box.AddComponent(
+        new AmmoBox(
+          this.scene,
+          this.assets["ammobox"].clone(),
+          this.assets["ammoboxShape"],
+          this.physicsWorld
+        )
+      );
       box.SetPosition(new THREE.Vector3(loc[0], loc[1], loc[2]));
       this.entityManager.Add(box);
     });
@@ -305,7 +376,9 @@ class FPSGameApp {
     this.entityManager.EndSetup();
 
     this.scene.add(this.camera);
-    this.animFrameId = window.requestAnimationFrame(this.OnAnimationFrameHandler);
+    this.animFrameId = window.requestAnimationFrame(
+      this.OnAnimationFrameHandler
+    );
   }
 
   StartGame = () => {
@@ -317,7 +390,7 @@ class FPSGameApp {
     this.SetupPhysics();
     this.EntitySetup();
     this.ShowMenu(false);
-  }
+  };
 
   // resize
   WindowResizeHanlder = () => {
@@ -325,10 +398,10 @@ class FPSGameApp {
     this.renderer.setSize(innerWidth, innerHeight);
     this.camera.aspect = innerWidth / innerHeight;
     this.camera.updateProjectionMatrix();
-  }
+  };
 
   // render loop
-  OnAnimationFrameHandler = (t) => {
+  OnAnimationFrameHandler = (t: any) => {
     if (this.lastFrameTime === null) {
       this.lastFrameTime = t;
     }
@@ -338,14 +411,16 @@ class FPSGameApp {
     this.Step(timeElapsed);
     this.lastFrameTime = t;
 
-    this.animFrameId = window.requestAnimationFrame(this.OnAnimationFrameHandler);
-  }
+    this.animFrameId = window.requestAnimationFrame(
+      this.OnAnimationFrameHandler
+    );
+  };
 
-  PhysicsUpdate = (world, timeStep) => {
+  PhysicsUpdate = (world: any, timeStep: any) => {
     this.entityManager.PhysicsUpdate(world, timeStep);
-  }
+  };
 
-  Step(elapsedTime) {
+  Step(elapsedTime: any) {
     this.physicsWorld.stepSimulation(elapsedTime, 10);
     //this.debugDrawer.update();
 
@@ -354,10 +429,9 @@ class FPSGameApp {
     this.renderer.render(this.scene, this.camera);
     this.stats.update();
   }
-
 }
 
 let _APP = null;
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
   _APP = new FPSGameApp();
 });
