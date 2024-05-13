ee77d089e6fe8d438cb69759c5a6b44cba2c9a83
Wei Ji (FlySkyPie) <c445dj544@gmail.com>
2024-05-13 21:39:01 +0800

    refactor: Convert file to typescript

--- src/AmmoLib.ts
+++ src/AmmoLib.ts
@@ -1,13 +1,13 @@
-import * as _Ammo from "ammo.js"
-import * as THREE from 'three'
-import { ConvexHull } from 'three/examples/jsm/math/ConvexHull'
+import * as _Ammo from "ammo.js";
+import * as THREE from "three";
+import { ConvexHull } from "three/examples/jsm/math/ConvexHull";
 
-let Ammo = null;
-let rayOrigin = null;
-let rayDest = null;
-let closestRayResultCallback = null;
+let Ammo: any = null;
+let rayOrigin: any = null;
+let rayDest: any = null;
+let closestRayResultCallback: any = null;
 
-const CollisionFlags = { CF_NO_CONTACT_RESPONSE: 4 }
+const CollisionFlags = { CF_NO_CONTACT_RESPONSE: 4 };
 const CollisionFilterGroups = {
   DefaultFilter: 1,
   StaticFilter: 2,
@@ -15,23 +15,23 @@ const CollisionFilterGroups = {
   DebrisFilter: 8,
   SensorTrigger: 16,
   CharacterFilter: 32,
-  AllFilter: -1 //all bits sets: DefaultFilter | StaticFilter | KinematicFilter | DebrisFilter | SensorTrigger
+  AllFilter: -1, //all bits sets: DefaultFilter | StaticFilter | KinematicFilter | DebrisFilter | SensorTrigger
 };
 
-function createConvexHullShape(object) {
+function createConvexHullShape(object: any) {
   const geometry = createConvexGeom(object);
   let coords = geometry.attributes.position.array;
   let tempVec = new Ammo.btVector3(0, 0, 0);
   let shape = new Ammo.btConvexHullShape();
   for (let i = 0, il = coords.length; i < il; i += 3) {
     tempVec.setValue(coords[i], coords[i + 1], coords[i + 2]);
-    let lastOne = (i >= (il - 3));
+    let lastOne = i >= il - 3;
     shape.addPoint(tempVec, lastOne);
   }
   return shape;
 }
 
-function createConvexGeom(object) {
+function createConvexGeom(object: any) {
   // Compute the 3D convex hull.
   let hull = new ConvexHull().setFromObject(object);
   let faces = hull.faces;
@@ -50,26 +50,31 @@ function createConvexGeom(object) {
   }
 
   const geom = new THREE.BufferGeometry();
-  geom.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-  geom.setAttribute('normal', new THREE.Float32BufferAttribute(normals, 3));
+  geom.setAttribute("position", new THREE.Float32BufferAttribute(vertices, 3));
+  geom.setAttribute("normal", new THREE.Float32BufferAttribute(normals, 3));
 
   return geom;
 }
 
 class AmmoHelper {
-
-  static Init(callback = () => { }) {
-    _Ammo.default.call(this).then((ammo) => {
+  static Init(callback = () => {}) {
+    _Ammo.default.call(this).then((ammo: any) => {
       Ammo = ammo;
       callback();
     });
   }
 
-  static CreateTrigger(shape, position, rotation) {
+  static CreateTrigger(shape: any, position: any, rotation: any) {
     const transform = new Ammo.btTransform();
     transform.setIdentity();
-    position && transform.setOrigin(new Ammo.btVector3(position.x, position.y, position.z));
-    rotation && transform.setRotation(new Ammo.btQuaternion(rotation.x, rotation.y, rotation.z, rotation.w));
+    position &&
+      transform.setOrigin(
+        new Ammo.btVector3(position.x, position.y, position.z)
+      );
+    rotation &&
+      transform.setRotation(
+        new Ammo.btQuaternion(rotation.x, rotation.y, rotation.z, rotation.w)
+      );
 
     const ghostObj = new Ammo.btPairCachingGhostObject();
     ghostObj.setCollisionShape(shape);
@@ -79,9 +84,12 @@ class AmmoHelper {
     return ghostObj;
   }
 
-  static IsTriggerOverlapping(ghostObj, rigidBody) {
+  static IsTriggerOverlapping(ghostObj: any, rigidBody: any) {
     for (let i = 0; i < ghostObj.getNumOverlappingObjects(); i++) {
-      const body = Ammo.castObject(ghostObj.getOverlappingObject(i), Ammo.btRigidBody);
+      const body = Ammo.castObject(
+        ghostObj.getOverlappingObject(i),
+        Ammo.btRigidBody
+      );
       if (body == rigidBody) {
         return true;
       }
@@ -90,15 +98,27 @@ class AmmoHelper {
     return false;
   }
 
-  static CastRay(world, origin, dest, result = {}, collisionFilterMask = CollisionFilterGroups.AllFilter) {
+  static CastRay(
+    world: any,
+    origin: any,
+    dest: any,
+    result: Record<string, any> = {},
+    collisionFilterMask = CollisionFilterGroups.AllFilter
+  ) {
     if (!rayOrigin) {
       rayOrigin = new Ammo.btVector3();
       rayDest = new Ammo.btVector3();
-      closestRayResultCallback = new Ammo.ClosestRayResultCallback(rayOrigin, rayDest);
+      closestRayResultCallback = new Ammo.ClosestRayResultCallback(
+        rayOrigin,
+        rayDest
+      );
     }
 
     // Reset closestRayResultCallback to reuse it
-    const rayCallBack = Ammo.castObject(closestRayResultCallback, Ammo.RayResultCallback);
+    const rayCallBack = Ammo.castObject(
+      closestRayResultCallback,
+      Ammo.RayResultCallback
+    );
     rayCallBack.set_m_closestHitFraction(1);
     rayCallBack.set_m_collisionObject(null);
 
@@ -107,14 +127,17 @@ class AmmoHelper {
     // Set closestRayResultCallback origin and dest
     rayOrigin.setValue(origin.x, origin.y, origin.z);
     rayDest.setValue(dest.x, dest.y, dest.z);
-    closestRayResultCallback.get_m_rayFromWorld().setValue(origin.x, origin.y, origin.z);
-    closestRayResultCallback.get_m_rayToWorld().setValue(dest.x, dest.y, dest.z);
+    closestRayResultCallback
+      .get_m_rayFromWorld()
+      .setValue(origin.x, origin.y, origin.z);
+    closestRayResultCallback
+      .get_m_rayToWorld()
+      .setValue(dest.x, dest.y, dest.z);
 
     // Perform ray test
     world.rayTest(rayOrigin, rayDest, closestRayResultCallback);
 
     if (closestRayResultCallback.hasHit()) {
-
       if (result.intersectionPoint) {
         const point = closestRayResultCallback.get_m_hitPointWorld();
         result.intersectionPoint.set(point.x(), point.y(), point.z());
@@ -127,12 +150,16 @@ class AmmoHelper {
 
       result.collisionObject = rayCallBack.get_m_collisionObject();
       return true;
-    }
-    else {
+    } else {
       return false;
     }
   }
-
 }
 
-export { AmmoHelper, Ammo, createConvexHullShape, CollisionFlags, CollisionFilterGroups }
+export {
+  AmmoHelper,
+  Ammo,
+  createConvexHullShape,
+  CollisionFlags,
+  CollisionFilterGroups,
+};
